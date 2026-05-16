85de20623d5f3db4ba6a3d45d8c4e80257be54a4
IWishThatICouldBeLikeTheCoolKids <40305079+DopeFiction@users.noreply.github.com>
2026-05-16 21:46:16 +0000

    added button to page

--- user-interface/app/Check-In/page.tsx
+++ user-interface/app/Check-In/page.tsx
@@ -1,11 +1,39 @@
+"use client";
+import { Layout, LayoutItem } from "../_components/elements/LayoutSystem";
+import { Button, Input, makeStyles } from '@fluentui/react-components';
+
+const useStyleList = makeStyles({
+    'row': {
+        'display': 'flex',
+        'gap': '0.5rem',
+        'alignItems': 'center',
+        'width': '100%'
+    },
+    'userIDEntry': {
+        'width': '70%'
+    }
+});
+
 /**
  * Check in page.
  * @returns Rendered check in page.
  */
 export default function Page(): React.ReactNode {
+    const styleList = useStyleList();
+
     return (
-        <>
-            Hello
-        </>
+        <Layout>
+            <>
+                Hello
+                 </>
+                <Layout >
+                    <LayoutItem className={ styleList.row }>
+                        <Input className={ styleList.userIDEntry } aria-label="Check in input" placeholder="Enter value" />
+                        <Button  appearance="primary" type="submit">Submit</Button>
+                    </LayoutItem>
+
+                </Layout>
+           
+        </Layout>
     );
 }

efb7d91e201f8e7cfbbd538653a26ff24fec3ce4
IWishThatICouldBeLikeTheCoolKids <40305079+DopeFiction@users.noreply.github.com>
2026-05-17 01:14:20 +0000

    added guid vlaidator

--- user-interface/app/Check-In/page.tsx
+++ user-interface/app/Check-In/page.tsx
@@ -1,6 +1,11 @@
 "use client";
 import { Layout, LayoutItem } from "../_components/elements/LayoutSystem";
-import { Button, Input, makeStyles } from '@fluentui/react-components';
+import { Button, Input, Text, makeStyles, tokens } from '@fluentui/react-components';
+import { useDispatch, useSelector } from 'react-redux';
+import { setInputValue, checkInInputSelector } from '../../store/components/elements/checkIn';
+import type { InputProps } from '@fluentui/react-components';
+
+const PATTERN = /^(?:urn:uuid:)?[0-9a-f]{8}-(?:[0-9a-f]{4}-){3}[0-9a-f]{12}$/i;
 
 const useStyleList = makeStyles({
     'row': {
@@ -11,6 +16,9 @@ const useStyleList = makeStyles({
     },
     'userIDEntry': {
         'width': '70%'
+    },
+    'successText': {
+        'color': tokens.colorStatusSuccessForeground1
     }
 });
 
@@ -20,6 +28,14 @@ const useStyleList = makeStyles({
  */
 export default function Page(): React.ReactNode {
     const styleList = useStyleList();
+    const dispatch = useDispatch();
+    const inputValue = useSelector(checkInInputSelector);
+
+    const isValid = PATTERN.test(inputValue);
+
+    const handleChange: InputProps['onChange'] = (_ev, data) => {
+        dispatch(setInputValue(data.value));
+    };
 
     return (
         <Layout>
@@ -28,10 +44,20 @@ export default function Page(): React.ReactNode {
                  </>
                 <Layout >
                     <LayoutItem className={ styleList.row }>
-                        <Input className={ styleList.userIDEntry } aria-label="Check in input" placeholder="Enter value" />
-                        <Button  appearance="primary" type="submit">Submit</Button>
+                        <Input
+                            className={ styleList.userIDEntry }
+                            aria-label="Check in input"
+                            placeholder="Enter value"
+                            value={ inputValue }
+                            onChange={ handleChange }
+                        />
+                        <Button appearance="primary" type="submit" disabled={ !isValid }>Submit</Button>
                     </LayoutItem>
-
+                    { isValid && (
+                        <LayoutItem>
+                            <Text className={ styleList.successText }>Valid ID entered</Text>
+                        </LayoutItem>
+                    ) }
                 </Layout>
            
         </Layout>
